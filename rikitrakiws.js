eed917bd42023b274b6782375ced058b51a2f6cc
Ricardo Morin <morinricardo@gmail.com>
2015-08-18 16:11:58 -0700

    Fix port

--- rikitrakiws.js
+++ rikitrakiws.js
@@ -2,7 +2,7 @@ var log4js = require('log4js');
 var logger = log4js.getLogger();
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var loglevel = process.env.LOGLEVEL || 'DEBUG';
 
 logger.setLevel(loglevel);
