43c76264ecb559e754a7e9dbc20b630526e1fdbc
Ricardo Morin <morinricardo@gmail.com>
2015-08-18 16:31:57 -0700

    Fix ipaddr

--- rikitrakiws.js
+++ rikitrakiws.js
@@ -3,6 +3,7 @@ var logger = log4js.getLogger();
 var express = require('express');
 var app = express();
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var loglevel = process.env.LOGLEVEL || 'DEBUG';
 
 logger.setLevel(loglevel);
@@ -11,6 +12,6 @@ app.use(log4js.connectLogger(log4js.getLogger('http'), { level: 'auto' }));
 
 app.use('/api/', require('./routes/'));
 
-app.listen(port);
+app.listen(port, ipaddress);
 
 logger.info('starting');
