dfb9b5b2b45d76a463af38eb6362922bf6ea9f9e
Ricardo Morin <morinricardo@gmail.com>
2015-11-09 18:21:55 -0800

    Drop header to avoid auth browser popup

--- rikitrakiws.js
+++ rikitrakiws.js
@@ -17,13 +17,12 @@ app.use(log4js.connectLogger(log4js.getLogger('http'), { level: 'auto' }));
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.raw({limit: '10mb', type: 'image/jpeg'}));
 
-
-app.use('/api/', require('./routes/').router);
-
 app.use(function (req, res, next) {
 	res.removeHeader("WWW-Authenticate");
 	next();
 });
+
+app.use('/api/', require('./routes/').router);
 
 /* app.use(function(error, req, res, next) {
 	if (error) {
