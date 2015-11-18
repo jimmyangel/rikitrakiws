a4ba93da18818fbd8776a6082dc470078fcba57a
Ricardo Morin <morinricardo@gmail.com>
2015-11-18 13:20:00 -0800

    Redirect to https when deployed in rikitraki domain

--- rikitrakiws.js
+++ rikitrakiws.js
@@ -8,6 +8,22 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var loglevel = process.env.LOGLEVEL || 'DEBUG';
 
 var app = express();
+
+// Enforce https redirect if appropriate
+app.enable('trust proxy');
+app.use(function (req, res, next) {
+	// Redirect to https only if we are in the rikitraki domain
+	if (/rikitraki.com/i.test(req.headers.host)) {
+		if (req.secure) {
+			next();
+		} else {
+			res.redirect('https://' + req.headers.host + req.url);
+		}
+	} else {
+		next();
+	}
+});
+
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 logger.setLevel(loglevel);
@@ -18,15 +34,6 @@ app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.raw({limit: '10mb', type: 'image/jpeg'}));
 
 app.use('/api/', require('./routes/').router);
-
-/* app.use(function(error, req, res, next) {
-	if (error) {
-		logger.error('InvalidInput', error.message);
-		res.status(error.status).send({error: 'InvalidInput', description: error.message});		
-	} else {
-		next();
-	}
-}); */
 
 app.listen(port, ipaddress, function () {
 	logger.info('starting rikitrakiws', this.address());
