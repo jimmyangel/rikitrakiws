32e8aed43349e8b7ce3f61256e48de83a7f33faa
Ricardo Morin <morinricardo@gmail.com>
2015-11-24 11:45:34 -0800

    Allow li tags in trail description

--- utils/utils.js
+++ utils/utils.js
@@ -3,7 +3,7 @@ var sanitizeHtml = require('sanitize-html');
 module.exports =  {
 	sanitize: function (dirty, restricted) {
 		if (dirty) {
-			var clean = sanitizeHtml(dirty, restricted ? {allowedTags: [], allowedAttributes: []} : {allowedTags: ['a'], allowedAttributes: {'a': [ 'href', 'target' ]}});
+			var clean = sanitizeHtml(dirty, restricted ? {allowedTags: [], allowedAttributes: []} : {allowedTags: ['a', 'li'], allowedAttributes: {'a': [ 'href', 'target' ]}});
 			return clean;
 		}
 	}
